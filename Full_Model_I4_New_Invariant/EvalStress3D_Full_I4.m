3dc5191d6dfadf60ec1d7f008089e5b0dc30c6fd
Mateo Martinez <mateo.martinezmejia@outlook.com>
2026-07-31 13:17:07 -0600

    Refactor EvalStress3D function for Mooney-Rivlin model

--- Full_Model_I4_New_Invariant/EvalStress3D_Full_I4.m
+++ Full_Model_I4_New_Invariant/EvalStress3D_Full_I4.m
@@ -1,14 +1,11 @@
-% Function EvalStress3D calculates the Cauchy Stress in the Lagrangian
-% configuration for each value of strain that we have in the Mechanical
-% Data. The variable "model" lets us choose what type of Strain-Energy
-% Density Function we want to use. Currently the only models are the
-% polynomic model ('Poly') and the exponential model ('Exp')
+% Function EvalStress3D calculates the Cauchy Stress for each value of 
+% strain that we have in the Mechanical Data. The variable "model" 
+% lets us choose what type of Strain-Energy Density Function we want to
+% use. Currently the only anisotropic model is the polynomic model ('Poly').
 
-% data = XRD data of fiber orientation
 % strain = array of strain percentages from mechanical data
 % c = coefficients for the model
 % model = array with model for isotropic and anisotropic SEDF
-% N = Degree of polynomial for each anisotropic invariant
 % M_ = Structure tensor
 
 function Stress = EvalStress3D_Full_I4(M_,strain,c,model)
@@ -18,24 +15,10 @@ Iso = model(1);
 Aniso = model(2);
 Stress = [];
 switch Iso
-    case 'NH' % Neo-Hookean model
-        %c4 = c(2:end);
+    case 'MR' % Mooney-Rivlin with parallel strain data
         for i=1:n
             Stress = [Stress;...
-                %CauchyStress3D_Full_I4(data,strain(i),c4,Aniso)];
-                NHCauchyStress3D_Full_I4(data,strain(i),c,Aniso)];
-        end
-    case 'MR' % Mooney-Rivlin model
-        %c4 = c(3:end);
-        for i=1:n
-            Stress = [Stress;...
-                %CauchyStress3D_Full_I4(data,strain(i),c4,Aniso)];
                 MRCauchyStress3D_Full_I4(M_,strain(i),c,Aniso)];
-        end
-    case 'NH_Perp' % Neo-Hookean with perpendicular strain data
-        for i=1:n
-            Stress = [Stress;...
-                NHCauchyStress3D_Full_I4_Perp(data,strain(i),c,Aniso)];
         end
     case 'MR_Perp' % Mooney-Rivlin with perpendicular strain data
         for i=1:n
