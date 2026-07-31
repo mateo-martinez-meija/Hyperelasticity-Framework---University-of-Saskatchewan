e276df28cdb3a2d7852ad3068634744a91d09946
Mateo Martinez <mateo.martinezmejia@outlook.com>
2026-07-31 14:40:41 -0600

    Refactor EvalSQfit function and update parameters

--- Full_Model_I4_No_Integration/EvalSQfit.m
+++ Full_Model_I4_No_Integration/EvalSQfit.m
@@ -3,14 +3,13 @@
 % data using the EvalStress function for the strain perpendicular to the
 % fibers. 
 
-% ODF = XRD data
+% theta = Fiber directions
 % s = strain perentages from mechanical data
 % c = coefficients for the model
 % model = array with model for isotropic and anisotropic SEDF
 % N = Degree of polynomial for each anisotropic invariant
-% M = Number of fiber families considered
 
-function eval = EvalSQfit(theta,s,c,model,N,M)
+function eval = EvalSQfit(theta,s,c,model,N)
 
 iso = model(1);
 aniso = model(2);
@@ -19,14 +18,9 @@ xPar = s(1:n/2);
 xPerp = s(1+n/2:end);
 
 switch iso
-    case "NH"
-        ParStress = EvalStress3D_Full_I4(theta,xPar,c,["NH",aniso]);
-        PerpStress = EvalStress3D_Full_I4(theta,xPerp,c,["NH_Perp",aniso]);
-
     case "MR"
-        ParStress = EvalStress3D_Full_I4(theta,xPar,c,["MR",aniso],N,M);
-        PerpStress = EvalStress3D_Full_I4(theta,xPerp,c,["MR_Perp",aniso],N,M);
-
+        ParStress = EvalStress3D_Full_I4(theta,xPar,c,["MR",aniso],N);
+        PerpStress = EvalStress3D_Full_I4(theta,xPerp,c,["MR_Perp",aniso],N);
 end
 eval = [ParStress;PerpStress];
 end
