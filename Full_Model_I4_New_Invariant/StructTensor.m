431d19bfe4b9d77bc2f57a2197f7a87977cf0fa4
Mateo Martinez <mateo.martinezmejia@outlook.com>
2026-07-31 13:43:55 -0600

    Refactor StructTensor function for clarity

--- Full_Model_I4_New_Invariant/StructTensor.m
+++ Full_Model_I4_New_Invariant/StructTensor.m
@@ -5,10 +5,8 @@ function M_ = StructTensor(psi,theta)
 
 % Calculate the structural tensor
 M_ = [0,0,0;0,0,0;0,0,0];
-%M_ = [0,0;0,0];
 for i=1:length(psi)-1
     n = [cos(theta(i)),sin(theta(i)),0]; % Fiber direction
-    %n = [cos(theta(i)),sin(theta(i))];
     delta = theta(i+1)-theta(i);
     M_ = M_ + psi(i)*delta*transpose(n)*n;
 end
