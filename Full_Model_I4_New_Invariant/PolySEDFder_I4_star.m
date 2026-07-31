44d8533a34aa1dcc0c7d15a5724bec12cbf16c7d
Mateo Martinez <mateo.martinezmejia@outlook.com>
2026-07-31 13:43:16 -0600

    Refine comments in PolySEDFder_I4_star function
    Updated comments for clarity and changed 'angle' to 'structure tensor' in function documentation.

--- Full_Model_I4_New_Invariant/PolySEDFder_I4_star.m
+++ Full_Model_I4_New_Invariant/PolySEDFder_I4_star.m
@@ -1,15 +1,13 @@
 % This function evaluates the derivative of a polynomic strain energy 
-% density function for a given value of strain, angle and set of 
+% density function for a given value of strain, structure tensor and set of 
 % coefficients.
 
-% n = direction of the fiber
+% M_ = Structure tensor
 % c = coefficients
 % C_ = Right Cauchy-Green tensor 
 
 function eval = PolySEDFder_I4_star(C_,M_,c)
 
-%C_ = [1/lambda,0,0;0,lambda^2,0;0,0,1/lambda];
-%n = [cos(theta_i),sin(theta_i),0];
 I4 = Inv4_star(C_,M_,"inv");
 
 eval = 0;
