cae0b2a4553d2bb3ed7bfa79de8ee4c72939a8b7
Mateo Martinez <mateo.martinezmejia@outlook.com>
2026-08-07 02:15:03 -0600

    Refactor EvalSQfit function for clarity and efficiency

--- Full_Model_I4_No_Integration/EvalSQfit.m
+++ Full_Model_I4_No_Integration/EvalSQfit.m
@@ -6,21 +6,16 @@
 % theta = Fiber directions
 % s = strain perentages from mechanical data
 % c = coefficients for the model
-% model = array with model for isotropic and anisotropic SEDF
 % N = Degree of polynomial for each anisotropic invariant
 
-function eval = EvalSQfit(theta,s,c,model,N)
+function eval = EvalSQfit(theta,s,c,N)
 
-iso = model(1);
-aniso = model(2);
 n = length(s);
 xPar = s(1:n/2);
 xPerp = s(1+n/2:end);
 
-switch iso
-    case "MR"
-        ParStress = EvalStress3D_Full_I4(theta,xPar,c,["MR",aniso],N);
-        PerpStress = EvalStress3D_Full_I4(theta,xPerp,c,["MR_Perp",aniso],N);
-end
+ParStress = EvalStress3D_Full_I4(theta,xPar,c,'parallel',N);
+PerpStress = EvalStress3D_Full_I4(theta,xPerp,c,'perpendicular',N);
+
 eval = [ParStress;PerpStress];
 end
