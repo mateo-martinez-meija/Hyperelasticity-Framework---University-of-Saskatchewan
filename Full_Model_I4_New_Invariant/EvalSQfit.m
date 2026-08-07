2d96c45c30ccfc905e1cc1fd6900bd7269793eb8
Mateo Martinez <mateo.martinezmejia@outlook.com>
2026-08-07 02:34:43 -0600

    Refactor EvalSQfit function for stress evaluation

--- Full_Model_I4_New_Invariant/EvalSQfit.m
+++ Full_Model_I4_New_Invariant/EvalSQfit.m
@@ -6,22 +6,16 @@
 
 % s = strain perentages from mechanical data
 % c = coefficients for the model
-% model = array with model for isotropic and anisotropic SEDF
 % M_ = Structure tensor
 
-function eval = EvalSQfit(M_,s,c,model)
+function eval = EvalSQfit(M_,s,c)
 
-iso = model(1);
-aniso = model(2);
 n = length(s);
 xPar = s(1:n/2);
 xPerp = s(1+n/2:end);
 
-switch iso
-
-    case "MR"
-        ParStress = EvalStress3D_Full_I4(M_,xPar,c,["MR",aniso]);
-        PerpStress = EvalStress3D_Full_I4(M_,xPerp,c,["MR_Perp",aniso]);
+ParStress = EvalStress3D_Full_I4(M_,xPar,c,'parallel');
+PerpStress = EvalStress3D_Full_I4(M_,xPerp,c,'perpendicular');
 
 end
 eval = [ParStress;PerpStress];
