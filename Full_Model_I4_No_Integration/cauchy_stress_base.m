004318811b87b3c1abfcf0ab0fdef40e12a57339
Mateo Martinez <mateo.martinezmejia@outlook.com>
2026-08-07 02:15:51 -0600

    Add files via upload

---
+++ Full_Model_I4_No_Integration/cauchy_stress_base.m
@@ -0,0 +1,29 @@
+
+
+
+function sigma_b = cauchy_stress_base(theta,c,F_,N)
+
+a = c(1);
+b = c(2);
+c4 = c(3:end); % Coefficients for I4
+
+S_ = [0,0,0;0,0,0;0,0,0];
+C_ = transpose(F_)*F_;
+
+for i=1:length(theta)
+    n = [cos(theta(i)),sin(theta(i)),0]; % Fiber direction
+    caux = c4(1:N-1); % Take only the coefficients for the fiber family
+    dI4 = Inv4(C_,n,"der");
+    S_ = S_ +...
+            PolySEDFder_I4(n,caux,C_)*dI4;
+    c4 = c4(N:end); % Update coefficients for next iteration
+end
+% We define the invariants I1 and I2
+dI1 = Inv1(C_,"der");
+dI2 = Inv2(C_,"der");
+
+% We include the isotropic component
+S_ = 2*(a*dI1 + b*dI2 + S_);
+sigma_b = F_*S_*transpose(F_);
+
+end
---
+++ Full_Model_I4_No_Integration/traction_residual.m
@@ -0,0 +1,28 @@
+% lambda1 = variable for traction-free condition
+% lambda = stretch
+% theta = angles for fiber families
+% c = coefficients
+% loadcase = direction of load (parallel or perpendicular)
+% N = degree of polynomial for anisotropic component
+
+function residual = traction_residual(theta,lambda1,lambda,c,loadcase,N)
+
+isPar = strcmp(loadcase,'parallel');
+
+lambda3 = 1/(lambda*lambda1);
+
+if isPar
+    F_ = diag([lambda1,lambda,lambda3]);
+    sigma_b = cauchy_stress_base(theta,c,F_,N);
+
+    residual = sigma_b(1,1) - sigma_b(3,3);
+
+else
+    F_ = diag([lambda,lambda1,lambda3]);
+    sigma_b = cauchy_stress_base(theta,c,F_,N);
+
+    residual = sigma_b(2,2) - sigma_b(3,3);
+end
+
+
+end
